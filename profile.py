7fb90fcc3c9c5d1350f982e6cc40d01a4a58f2ac
Семен <s.ojereliew@yandex.ru>
2020-06-18 18:01:03 +0300

    ДЗ 3

---
+++ profile.py
@@ -0,0 +1,13 @@
+
+class Profile:
+    def __init__(self, firstname, lastname, nickname, address, mobile, email, bday, bmonth, byear, address2):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.address2 = address2
