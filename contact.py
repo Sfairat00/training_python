00f4d65a4a9b108923e99518e416a621844e6ae8
Семен <s.ojereliew@yandex.ru>
2020-06-18 17:34:30 +0300

    Добавил параметры

---
+++ contact.py
@@ -0,0 +1,13 @@
+
+class Contact:
+    def __init__(self, firstname, lastname, nickname, address, mobile, email, bday, bmounth, byear, address2):
+        self.firsrname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmounth = bmounth
+        self.byear = byear
+        self.address2 = address2
