a85a9be76a135ee2264870beea52ffd74d2d20db
Семен <s.ojereliew@yandex.ru>
2020-07-01 22:43:44 +0300

    Повысили интелектуальность фикстуры

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,14 +3,10 @@ from model.profile import Profile
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.profile.create_contact(Profile(firstname="Saimon", lastname="Ozhereliev", nickname="Sfai", address="Moscow", mobile="916 176-66-66", email="s.ojereliew@yandex.ru",
                                        bday="18", bmonth="April", byear="1986", address2="Moscow city"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.profile.create_contact(Profile(firstname="", lastname="", nickname="", address="", mobile="", email="",
                                        bday="", bmonth="", byear="", address2=""))
-    app.session.logout()
