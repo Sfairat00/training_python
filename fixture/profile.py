6acf6a553c7009acf616bec2ccaa8b992f39e905
Семен <s.ojereliew@yandex.ru>
2020-07-12 21:50:11 +0300

    ДЗ 10

--- fixture/profile.py
+++ fixture/profile.py
@@ -61,7 +61,8 @@ class ProfileHelper:
 
     def open_new_conttact(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def delete_first_contact(self):
         wd = self.app.wd
